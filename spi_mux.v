c151cd1f42e5d591d38c56829420c197781c0a34
Tristan Seifert <tristan@tseifert.me>
2018-10-26 21:35:11 -0500

    reimplement with state machin

--- spi_mux.v
+++ spi_mux.v
@@ -1,100 +1,99 @@
 module spi_mux(clk, reset, spi_nCS, spi_sck, spi_mosi, spi_miso, out, status, buffer_oe, out_en);
 
-// declare inputs
+// master clock (50MHz)
 input				clk;
+// active low reset line
 input				reset;
 
+// SPI chip select
 input				spi_nCS;
+// SPI clock
 input				spi_sck;
+// SPI master output
 input				spi_mosi;
 
-// declare outputs
+// SPI master input
 output			spi_miso;
 
+// output bus
 output	[7:0]	out;
 reg		[7:0] out;
 
+// status LEDs
 output	[2:0] status;
+reg		[2:0] status;
 
+// output enable for 3v3 -> 5v translator
 output			buffer_oe;
+reg				buffer_oe;
 
+// output enable for differential driver
 output	[3:0]	out_en;
+reg		[3:0] out_en;
 
-// when asserted, shift reg data is latched to outputs
-wire				latch_out;
-// output bus of shift register
-wire		[7:0] shift_q;
-//reg		[7:0]	shift_q;
 
-// resets shift register
-wire				shift_reset;
-// shift register clock (gated SCLK)
-wire				shift_clk;
+// state
+parameter	STATE_IDLE	= 4'b0000;
+parameter	STATE_SHIFT	= 4'b0001;
+
+reg		[3:0] state = STATE_IDLE;
 
 // counter for pulses of SCK
-reg		[4:0]	sck_counter;
+reg		[3:0]	sck_counter;
 
 
-// shift register's clock is SCK but ANded with CS
-assign shift_clk = spi_sck & !spi_nCS;
 
-// copy shift register data bus when latch is asserted
-always @ (latch_out, shift_q)
+always @(posedge spi_sck)
 begin
-	if (latch_out == 1) begin
-		out = shift_q;
-	end
-end
+	// the bus voltage translator is always active
+	buffer_oe <= 1;
+	
+	// status 0 is the reset bit but inverted
+	status[0] <= ~reset;
 
-// count the number of pulses on the clock; when 8 assert the latch
-always @ (spi_sck, spi_nCS, reset)
-begin
 	// is reset asserted?
-	if (reset == 0) begin
-		// reset counter
+	if(~reset) begin
+		// clear the output data bus
+		out <= 0;
+		// disable differential drivers
+		out_en <= 0;
+		
+		// clear the counter and state
+		state <= STATE_IDLE;
 		sck_counter <= 0;
-	end
-	// it isn't, handle normal
-	else
-		// latch deasserted if CS goes deasserted
-		if (spi_nCS == 1) begin
-			// not selected so unlatch
-			latch_out <= 0;
-			
-			// clear counter
-			sck_counter <= 0;
-		end
-		else
-			// otherwise, count SCK pulses
-			if (posedge spi_sck) begin				
-				// if it's 8, assert latch
-				if(sck_counter = 8) begin
-					latch_out <= 1;
-					
-					// reset counter
-					sck_counter <= 0;
-				else
-					// reset latch tho
-					latch_out <= 0;
-				
-					// otherwise, just increment the counter
-					sck_counter <= sck_counter + 1;
+	end else begin
+		// reset is not asserted, do state machine-y stuff
+		case (state)
+			// idle state, wait for CS to be asserted
+			STATE_IDLE: begin
+				if(~spi_nCS) begin
+					state <= STATE_SHIFT;
 				end
 			end
-		end
+			
+			// shift bits in until CS goes high
+			STATE_SHIFT: begin
+				// is CS high now?
+				if(~spi_nCS) begin
+					// if so, increment the counter
+					sck_counter <= sck_counter + 1;
+						
+					// now, copy the value to the appropriate output
+					out[sck_counter] = spi_mosi;
+					
+					// stay in this state
+					state <= STATE_SHIFT;
+				end else begin
+					// otherwise, go back to the idle state
+					state <= STATE_IDLE;
+					
+					// be sure to reset counter
+					sck_counter <= 0;
+				end
+			end
+		endcase
 	end
 end
 
-
-
-// instantiate shift register
-shiftreg shiftyboi (
-	.aset			(shift_reset),
-	.clock		(shift_clk),
-	// shift input is SPI MOSI
-	.shiftin		(spi_mosi),
-	// connect output bus
-	.q				(shift_q)
-);
 
 endmodule
